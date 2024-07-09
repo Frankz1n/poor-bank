90a0bcf34134212fce50f3632517f2ed7c1591a7
franklyn <franklynlima016@gmail.com>
2024-07-09 19:33:15 -0300

    feat: criação da pagina de investimento

--- poor-bank/src/main.jsx
+++ poor-bank/src/main.jsx
@@ -8,17 +8,20 @@ import Login from "./pages/Login/login";
 import UserScreen from "./pages/PrincipalScreen/principalScreen";
 import Ticket from "./pages/Ticket/ticket";
 import Register from "./pages/Register/register";
+import Investing from "./pages/Investing/Investing";
+import Pix from "./pages/Pix/Pix";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="userscreen" element={<UserScreen />} />
-        <Route path="login" element={<Login />} />
         <Route path="registro" element={<Register />} />
-        <Route path="ticket" element={<Ticket />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/userscreen" element={<UserScreen />} />
+        <Route path="/investimento" element={<Investing />} />
+        <Route path="/pix" element={<Pix />} />
+        <Route path="/ticket" element={<Ticket />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ poor-bank/src/pages/Investing/Investing.jsx
@@ -0,0 +1,55 @@
+import styles from "./Investing.module.css";
+
+import HeaderDefault from "../../components/HeaderDefault/HeaderDefault";
+import cofrinho from "../../assets/cofrinho.png";
+import investGraf from "../../assets/investimentoGrafico.png";
+import InputDefault from "../../components/InputDefault/InputDefault";
+import ButtonDefault from "../../components/ButtonDefault/ButtonDefault";
+
+export default function Investing() {
+  return (
+    <div>
+      <HeaderDefault />
+      <div className={styles.container}>
+        <div className={styles.body}>
+          <div className={styles.top}>
+            <div className={styles.balance}>
+              <div>SALDO</div>
+              <div>R$ 225,25</div>
+            </div>
+          </div>
+          <div className={styles.bottom}>
+            <h1>Como você quer investir?</h1>
+            <div className={styles.bottomOP}>
+              <div className={styles.vault}>
+                <div className={styles.vaultCofrinho}>
+                  <img src={cofrinho} alt="cofrinho icon" /> <h2>Cofrinho</h2>
+                </div>
+                <InputDefault
+                  type="text"
+                  name="inputCofrinho"
+                  width="250px"
+                  placeholder="Valor que deseja investir"
+                />
+                <ButtonDefault label="investir" width="150px" height="50px" />
+              </div>
+              <div className={styles.cdb}>
+                <div className={styles.cdbInvest}>
+                  <img src={investGraf} alt="investimento icon" />
+                  <h2>CDB Diário</h2>
+                </div>
+                <InputDefault
+                  type="text"
+                  name="inputInvestimento"
+                  width="250px"
+                  placeholder="Valor que deseja investir"
+                />
+                <ButtonDefault label="investir" width="150px" height="50px" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
