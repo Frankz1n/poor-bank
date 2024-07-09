5908ab31c8cbc1b92657769ef6a79a5f69bfb3fc
franklyn <franklynlima016@gmail.com>
2024-07-09 18:45:29 -0300

    feat: finalização da pagina de usuario logado

--- poor-bank/src/components/ButtonDefault/ButtonDefault.jsx
+++ poor-bank/src/components/ButtonDefault/ButtonDefault.jsx
@@ -1,9 +1,13 @@
 import style from "./ButtonDefault.module.css";
 
 export default function ButtonDefault({ label, width, height }) {
-    return (
-        <div className={style.container} onChange={(e) => setInputValue(e.target.value)} style={{ width: width, height: height}} >
-            <button className={style.button}  > {label} </button>
-        </div>
-    );
+  return (
+    <div
+      className={style.container}
+      onChange={(e) => setInputValue(e.target.value)}
+      style={{ width: width, height: height }}
+    >
+      <button className={style.button}> {label} </button>
+    </div>
+  );
 }
--- poor-bank/src/pages/PrincipalScreen/principalScreen.jsx
+++ poor-bank/src/pages/PrincipalScreen/principalScreen.jsx
@@ -3,6 +3,11 @@ import ItemBox from "../../components/ItemBox/ItemBox";
 import styles from "./principalScreen.module.css";
 
 import scam from "../../assets/scanear.png";
+import pix from "../../assets/pixIcon.png";
+import transfer from "../../assets/transferIcon.png";
+import invest from "../../assets/investIcon.png";
+import emprest from "../../assets/emprestIcon.png";
+import ButtonDefault from "../../components/ButtonDefault/ButtonDefault";
 
 export default function UserScreen() {
   return (
@@ -20,12 +25,27 @@ export default function UserScreen() {
         </div>
         <div className={styles.midle}>
           <ItemBox img={scam} text="Pagar Boleto" />
-          <ItemBox img={scam} text="Pagar Boleto" />
-          <ItemBox img={scam} text="Pagar Boleto" />
-          <ItemBox img={scam} text="Pagar Boleto" />
-          <ItemBox img={scam} text="Pagar Boleto" />
+          <ItemBox img={pix} text="Área Pix" />
+          <ItemBox img={transfer} text="Transferir" />
+          <ItemBox img={invest} text="Investir" />
+          <ItemBox img={emprest} text="Pedir Empréstimo" />
         </div>
-        <div className={styles.bottom}></div>
+        <div className={styles.separation}></div>
+        <div className={styles.bottom}>
+          <div className={styles.bottomContent1}>
+            <div className={styles.information}>
+              <h2>Cartão de crédito</h2>
+              <div className={styles.invoice}>
+                <h3>Fatura atual</h3>
+                <h2>R$ 225,00</h2>
+              </div>
+            </div>
+          </div>
+          <div className={styles.bottomContent2}>
+            <ButtonDefault label="Pagar Cartão" width="150px" height="40px" />
+            <div className={styles.bottomContent2Arrow}>{">"}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
